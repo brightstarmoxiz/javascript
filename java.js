a446055ad7a591a6f1ad7cd37935aaae6d2bbdf8
brightstar <jojololamoses@gmail.com>
2022-06-06 22:43:11 +0100

    javascript tutorial

---
+++ java.js
@@ -0,0 +1,48 @@
+// alert("hello world")
+
+//     function greet() {
+//       let name=document.getElementsById('myname').value
+//       alert(name)
+      
+//   }
+
+
+  
+  function hey(params) {
+    document.getElementById (`hey`).innerHTML = `This is brightstar`
+};
+
+function hi(params) {
+    document.getElementById (`hey`).innerHTML = ``
+};
+function star(params) {
+   document.getElementById (`star`).innerHTML='http.//.www.brightstarmoxiz.com'
+    
+};
+function moon(params) {
+    document.getElementById (`star`).innerHTML = ``
+};
+
+
+// bulb light
+function myImage(params) {
+document.getElementById('myImage').src=`../myjavscrp.js/pic_bulbon.gif`
+};
+function mylife(params) {
+   document.getElementById('myImage').src=`../myjavscrp.js/pic_bulboff (1).gif`
+};
+
+//  function
+
+function sum(num,nim2,num3,num4,) {
+    alert(num+nim2*num3-num4-num5)
+}
+sum(10,20,19,20,)
+
+alert
+
+function greet(my,me,a) {
+   alert(my+me+a)
+}
+greet(`hello,you are welcome`, ` brightstar`, ` mozix `)
+
